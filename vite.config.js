634ff81aaaad1ad32a7da3b73ba585b859fe201e
muhamiyan <apriliansyahidris@gmail.com>
2024-08-09 07:28:33 +0700

    chore: update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { getProxyOptions } from 'frappe-ui/src/utils/vite-dev-server'
-import { webserver_port } from '../../../sites/common_site_config.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 8080,
-    proxy: getProxyOptions({ port: webserver_port }),
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
